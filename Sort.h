b6b9dc261957b9775e51fa02c10e506d027f4cf4
Seesax <58180801+Seesax@users.noreply.github.com>
2022-12-06 15:54:19 +0700

    Delete Sort.h

--- Sort.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Book.h"
-#include <vector>
-using namespace std;
-void SortById(vector<BookRating>& arr, int digits);
-void PushBack(vector<BookRating>& arr, BookRating a);
-void SortArr(vector<BookRating>& arr, vector<vector <BookRating>>& typeArr);
